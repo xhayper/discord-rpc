e3d13ab2a38374d5e795557fd5f7ebb16a85fbc2
xhayper <hayper1919@gmail.com>
2022-07-29 15:33:26 +0700

    chore: Pretty thing up

--- src/structures/user.ts
+++ src/structures/user.ts
@@ -1,17 +1,17 @@
 export class User {
-  id: string = '';
-  username: string = '';
-  discriminator: string = '';
-  avatar: string = '';
-  bot: boolean = false;
-  flags: number = 0;
-  premium_type: number = 0;
+    id: string = "";
+    username: string = "";
+    discriminator: string = "";
+    avatar: string = "";
+    bot: boolean = false;
+    flags: number = 0;
+    premium_type: number = 0;
 
-  constructor(props: object) {
-    Object.assign(this, props);
-  }
+    constructor(props: object) {
+        Object.assign(this, props);
+    }
 
-  get tag() {
-    return `${this.username}#${this.discriminator}`;
-  }
+    get tag() {
+        return `${this.username}#${this.discriminator}`;
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,23 @@
-import { Client } from '../src/index';
+import { Client } from "../src/index";
 
 (async () => {
-  const client = new Client({ clientId: '964574770807058472', transport: { type: 'websocket' } });
-
-  client.on('ready', () => {
-    client.user?.setActivity({
-      state: 'Hello, world!',
-      buttons: [
-        {
-          label: 'Hi!',
-          url: 'https://github.com/xhayper'
-        }
-      ],
-      startTimestamp: new Date()
+    const client = new Client({
+        clientId: "964574770807058472",
+        transport: { type: "websocket" }
     });
-  });
 
-  client.login({ scopes: ['rpc'] });
+    client.on("ready", () => {
+        client.user?.setActivity({
+            state: "Hello, world!",
+            buttons: [
+                {
+                    label: "Hi!",
+                    url: "https://github.com/xhayper"
+                }
+            ],
+            startTimestamp: new Date()
+        });
+    });
+
+    client.login({ scopes: ["rpc"] });
 })();

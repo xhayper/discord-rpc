51014b5147b433f3b1ada791a6a955afde5e0f24
xhayper <hayper1919@gmail.com>
2022-07-29 12:55:27 +0700

    test: Seperate folder

---
+++ tests/index.ts
@@ -0,0 +1,21 @@
+import { Client } from "../src/index";
+
+(async () => {
+    const client = new Client({clientId: "964574770807058472"});
+
+    client.on("ready", () => {
+        console.log(client.user, client.user?.tag);
+
+        client.user?.setActivity({
+            state: "Hello, world!",
+            buttons: [
+                {
+                    "label": "Hi!",
+                    "url": "https://github.com/xhayper"
+                }
+            ]
+        })
+    });
+
+    client.login();
+})();

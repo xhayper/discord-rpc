c66c9e350adcc70ac80e591bbaa478f16bcd8609
xhayper <46065349+xhayper@users.noreply.github.com>
2022-07-29 15:25:12 +0700

    feat: Fix stuff up

--- src/transport/ipc.ts
+++ src/transport/ipc.ts
@@ -1,17 +1,17 @@
-import { Client } from "..";
-import { CMD, CommandIncoming, EVT, Transport } from "../structures/transport";
-import fs from "fs";
-import path from "path";
-import net from "net";
-import { v4 as uuidv4 } from "uuid";
-import axios from "axios";
+import { Client } from '../client';
+import { CommandIncoming, Transport } from '../structures/transport';
+import fs from 'fs';
+import path from 'path';
+import net from 'net';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 export enum OPCODE {
-    HANDSHAKE,
-    FRAME,
-    CLOSE,
-    PING,
-    PONG
+  HANDSHAKE,
+  FRAME,
+  CLOSE,
+  PING,
+  PONG
 }
 
 /*
@@ -24,149 +24,158 @@ HANDSHAKE 40bytes  { " v " : 1 , "
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 export type IPCTransportOptions = {
-    formatPathFunction?: (id: number, snap?: boolean) => string;
-}
+  formatPathFunction?: (id: number, snap?: boolean) => string;
+};
 
 export const createPacket = (opcode: OPCODE, data?: any): Buffer => {
-    const dataBuffer = data ? Buffer.from(JSON.stringify(data)) : Buffer.alloc(0);
+  const dataBuffer = data ? Buffer.from(JSON.stringify(data)) : Buffer.alloc(0);
 
-    const packet = Buffer.alloc(8);
-    packet.writeUInt32LE(opcode, 0);
-    packet.writeUInt32LE(dataBuffer.length, 4);
+  const packet = Buffer.alloc(8);
+  packet.writeUInt32LE(opcode, 0);
+  packet.writeUInt32LE(dataBuffer.length, 4);
 
-    return Buffer.concat([packet, dataBuffer]);
-}
+  return Buffer.concat([packet, dataBuffer]);
+};
 
-export const parsePacket = (packet: Buffer): { op: OPCODE, length: number, data?: CommandIncoming } => {
-    const op = packet.readUInt32LE(0);
-    const length = packet.readUInt32LE(4);
-    const data = length > 0 ? JSON.parse(packet.subarray(8).toString()) : null;
+export const parsePacket = (packet: Buffer): { op: OPCODE; length: number; data?: CommandIncoming } => {
+  const op = packet.readUInt32LE(0);
+  const length = packet.readUInt32LE(4);
+  const data = length > 0 ? JSON.parse(packet.subarray(8).toString()) : null;
 
-    return { op, length, data };
-}
+  return { op, length, data };
+};
 
 const formatPath = (id: number, snap: boolean = false): string => {
-    if (process.platform === 'win32') return `\\\\?\\pipe\\discord-ipc-${id}`;
+  if (process.platform === 'win32') return `\\\\?\\pipe\\discord-ipc-${id}`;
 
-    const { env: { XDG_RUNTIME_DIR, TMPDIR, TMP, TEMP } } = process;
-    const prefix = fs.realpathSync(XDG_RUNTIME_DIR || TMPDIR || TMP || TEMP || `${path.sep}tmp`);
-    return `${prefix}${snap ? `${path.sep}snap.discord` : ""}${path.sep}discord-ipc-${id}`;
-}
+  const {
+    env: { XDG_RUNTIME_DIR, TMPDIR, TMP, TEMP }
+  } = process;
+  const prefix = fs.realpathSync(XDG_RUNTIME_DIR || TMPDIR || TMP || TEMP || `${path.sep}tmp`);
+  return `${prefix}${snap ? `${path.sep}snap.discord` : ''}${path.sep}discord-ipc-${id}`;
+};
 
 const createSocket = async (path: string): Promise<net.Socket> => {
-    return new Promise((resolve, reject) => {
-        const onError = () => {
-            socket.removeListener("conect", onConnect);
-            reject();
-        };
+  return new Promise((resolve, reject) => {
+    const onError = () => {
+      socket.removeListener('conect', onConnect);
+      reject();
+    };
 
-        const onConnect = () => {
-            socket.removeListener("error", onError);
-            resolve(socket);
-        }
+    const onConnect = () => {
+      socket.removeListener('error', onError);
+      resolve(socket);
+    };
 
-        const socket = net.createConnection(path);
+    const socket = net.createConnection(path);
 
-        socket.once("connect", onConnect)
-        socket.once("error", onError);
-    });
+    socket.once('connect', onConnect);
+    socket.once('error', onError);
+  });
 };
 
 export class IPCTransport extends Transport {
-    options: IPCTransportOptions;
-    private socket?: net.Socket;
+  options: IPCTransportOptions;
+  private socket?: net.Socket;
 
-    constructor(client: Client, options?: IPCTransportOptions) {
-        super(client)
+  constructor(client: Client, options?: IPCTransportOptions) {
+    super(client);
 
-        this.options = options || {};
-    }
+    this.options = options || {};
+  }
 
-    private async getSocket(): Promise<net.Socket> {
-        if (this.socket) return this.socket;
+  private async getSocket(): Promise<net.Socket> {
+    if (this.socket) return this.socket;
 
-        const formatFunc = this.options.formatPathFunction || formatPath;
-        return new Promise(async (resolve, reject) => {
+    const formatFunc = this.options.formatPathFunction || formatPath;
+    return new Promise(async (resolve, reject) => {
+      for (let i = 0; i < 10; i++) {
+        let socket = await createSocket(formatFunc(i)).catch(() => null);
+
+        if (socket) {
+          resolve(socket);
+          return;
+        } else if (process.platform === 'linux') {
+          socket = await createSocket(formatFunc(i, true)).catch(() => null);
+          if (socket) {
+            resolve(socket);
+            return;
+          }
+        }
+      }
+
+      reject(new Error('Could not connect'));
+    });
+  }
+
+  async connect(): Promise<void> {
+    this.socket = await this.getSocket().catch((err) => {
+      throw err;
+    });
+
+    this.emit('open');
+
+    this.send(
+      {
+        v: 1,
+        client_id: this.client.clientId
+      },
+      OPCODE.HANDSHAKE
+    );
+
+    this.socket.pause();
+
+    this.socket.on('readable', async () => {
+      if (!this.socket) return;
+
+      const data = this.socket.read();
+      if (!data) return;
+
+      const packet = parsePacket(data);
+      console.log(packet);
+
+      switch (packet.op) {
+        case OPCODE.FRAME: {
+          if (!packet.data) break;
+
+          if (packet.data.cmd === 'AUTHORIZE' && packet.data.evt !== 'ERROR') {
             for (let i = 0; i < 10; i++) {
-                let socket = await createSocket(formatFunc(i)).catch(() => null);
-
-                if (socket) {
-                    resolve(socket);
-                    return;
-                } else if (process.platform === 'linux') {
-                    socket = await createSocket(formatFunc(i, true)).catch(() => null);
-                    if (socket) { resolve(socket); return; }
-                }
+              const response = await axios.get(`http://127.0.0.1:${6463 + i}`).catch(() => null);
+              if (!response || response.status == 404) continue;
+              this.client.endPoint = `http://127.0.0.1:${6463 + i}`;
             }
+          }
 
-            reject(new Error('Could not connect'));
-        });
-    }
+          this.emit('message', packet.data);
+          break;
+        }
+        case OPCODE.CLOSE: {
+          this.emit('close');
+          break;
+        }
+        case OPCODE.PING: {
+          this.send(packet.data, OPCODE.PONG);
+          this.emit('ping');
+          break;
+        }
+      }
+    });
+  }
 
-    async connect(): Promise<void> {
-        this.socket = await this.getSocket().catch((err) => { throw err });
+  send(message?: any, op: OPCODE = OPCODE.FRAME): void {
+    console.log(message, op);
+    this.socket?.write(createPacket(op, message));
+  }
 
-        this.emit("open");
+  ping(): void {
+    this.send(uuidv4(), OPCODE.PING);
+  }
 
-        this.send({
-            v: 1,
-            client_id: this.client.clientId,
-        }, OPCODE.HANDSHAKE);
-
-        this.socket.pause();
-
-        this.socket.on("readable", async () => {
-            if (!this.socket) return;
-
-            const data = this.socket.read();
-            if (!data) return;
-
-            const packet = parsePacket(data);
-            console.log(packet);
-
-            switch (packet.op) {
-                case OPCODE.FRAME: {
-                    if (!packet.data) break;
-
-                    if (packet.data.cmd === 'AUTHORIZE' && packet.data.evt !== 'ERROR') {
-                        for (let i = 0; i < 10; i++) {
-                            const response = await axios.get(`http://127.0.0.1:${6463 + i}`).catch(() => null);
-                            if (!response || response.status == 404) continue;
-                            this.client.endPoint = `http://127.0.0.1:${6463 + i}`;
-                        }
-                    }
-
-                    this.emit("message", packet.data);
-                    break;
-                }
-                case OPCODE.CLOSE: {
-                    this.emit("close");
-                    break;
-                }
-                case OPCODE.PING: {
-                    this.send(packet.data, OPCODE.PONG);
-                    this.emit("ping");
-                    break;
-                }
-            }
-        });
-    }
-
-    send(message?: any, op: OPCODE = OPCODE.FRAME): void {
-        console.log(message, op);
-        this.socket?.write(createPacket(op, message));
-    }
-
-    ping(): void {
-        this.send(uuidv4(), OPCODE.PING);
-    }
-
-    close(): Promise<void> {
-        return new Promise((resolve) => {
-            this.once('close', resolve);
-            this.send({}, OPCODE.CLOSE);
-            this.socket?.end();
-        })
-    }
-
+  close(): Promise<void> {
+    return new Promise((resolve) => {
+      this.once('close', resolve);
+      this.send({}, OPCODE.CLOSE);
+      this.socket?.end();
+    });
+  }
 }

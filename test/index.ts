1ca6fb313f9e146f51be8bd22ead155938d0a8c6
xhayper <hayper1919@gmail.com>
2022-07-30 10:48:46 +0700

    feat: Update test

--- test/index.ts
+++ test/index.ts
@@ -1,23 +1,20 @@
 import { Client } from "../src/index";
 
-(async () => {
-    const client = new Client({
-        clientId: "964574770807058472",
-        transport: { type: "websocket" }
-    });
+const client = new Client({
+    clientId: "964574770807058472"
+});
 
-    client.on("ready", () => {
-        client.user?.setActivity({
-            state: "Hello, world!",
-            buttons: [
-                {
-                    label: "Hi!",
-                    url: "https://github.com/xhayper"
-                }
-            ],
-            startTimestamp: new Date()
-        });
+client.on("ready", () => {
+    client.user?.setActivity({
+        state: "Hello, world!",
+        buttons: [
+            {
+                label: "Hi!",
+                url: "https://github.com/xhayper"
+            }
+        ],
+        startTimestamp: new Date()
     });
+});
 
-    client.login({ scopes: ["rpc"] });
-})();
+client.login();

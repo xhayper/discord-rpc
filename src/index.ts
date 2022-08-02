511f6896dc1204ec9d40ce4d28f670937aec586d
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-02 14:45:58 +0700

    feat: improve backend

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./structures/Transport";
 export * from "./client";
 
 export type { SetActivity, SetActivityResponse } from "./structures/ClientUser";
-export type { Transport, CMD, EVT } from "./structures/Transport";
+export type { Transport, TransportOptions, CMD, EVT } from "./structures/Transport";
 export type { FormatFunction } from "./transport/ipc";

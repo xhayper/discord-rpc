26240d6f6f54f582daf8c2543f3cd44b169dcec1
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-07 14:01:49 +0700

    feat: exported more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "./structures/Transport";
 export * from "./Client";
 
-export type { SetActivity, SetActivityResponse } from "./structures/ClientUser";
-export type { Transport, TransportOptions, CMD, EVT, CommandOutgoing, CommandIncoming } from "./structures/Transport";
-export type { FormatFunction } from "./transport/IPC";
+export { User } from "./structures/User";
+export { ClientUser, SetActivity, SetActivityResponse } from "./structures/ClientUser";
+export { Transport, TransportOptions, CMD, EVT, CommandOutgoing, CommandIncoming } from "./structures/Transport";
+export { FormatFunction } from "./transport/IPC";

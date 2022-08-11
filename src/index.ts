38fe2226b4cc623520548a47fdd8183a7d9adf6b
xhayper <46065349+xhayper@users.noreply.github.com>
2022-08-11 12:36:22 +0700

    feat: export Lobby

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./structures/ClientUser";
 export * from "./structures/CertifiedDevice";
 export * from "./structures/Channel";
 export * from "./structures/Guild";
+export * from "./structures/Lobby";
 export * from "./structures/User";
 export * from "./structures/VoiceSettings";
 export * from "./structures/Transport";

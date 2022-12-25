39deb632440d5f4527bb8bfd41903b3997b39d63
xhayper <46065349+xhayper@users.noreply.github.com>
2022-12-25 16:28:22 +0700

    fix: a bug where socket getting disconnect won't kill socket

--- src/transport/IPC.ts
+++ src/transport/IPC.ts
@@ -208,6 +208,7 @@ export class IPCTransport extends Transport {
         });
 
         this.socket.on("close", () => {
+            this.socket = undefined;
             this.emit("close", "Closed by Discord");
         });
     }
